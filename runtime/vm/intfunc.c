9909aa24586544ebb2017bbd04cd8bb80f84832f
Cheng Jin <jincheng@ca.ibm.com>
2022-01-26 11:10:22 -0500

    JEP389/419/424 Foreign Linker API: UpCall (primitive & struct support)
    The change aims to support the upcall handler to the requested
    java method via the native thunk generated by JIT.
    
    Signed-off-by: Cheng Jin <jincheng@ca.ibm.com>

--- runtime/vm/intfunc.c
+++ runtime/vm/intfunc.c
@@ -416,4 +416,16 @@ J9InternalVMFunctions J9InternalFunctions = {
 #if JAVA_SPEC_VERSION >= 19
 	createContinuation,
 #endif /* JAVA_SPEC_VERSION >= 19 */
+#if JAVA_SPEC_VERSION >= 16
+	createUpcallThunk,
+	getArgPointer,
+	allocateUpcallThunkMemory,
+	doneUpcallThunkGeneration,
+	icallVMprJavaUpcall0,
+	icallVMprJavaUpcall1,
+	icallVMprJavaUpcallJ,
+	icallVMprJavaUpcallF,
+	icallVMprJavaUpcallD,
+	icallVMprJavaUpcallStruct,
+#endif /* JAVA_SPEC_VERSION >= 16 */
 };
