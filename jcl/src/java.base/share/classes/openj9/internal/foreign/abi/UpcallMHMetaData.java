c60258d2633b8102cdd84e112eda8aade579efe8
Cheng Jin <jincheng@ca.ibm.com>
2022-01-26 11:10:22 -0500

    JEP389/419/424 Foreign Linker API: UpCall (primitive & struct support)
    The change aims to support the upcall handler to the requested
    java method via the native thunk generated by JIT.
    
    Signed-off-by: Cheng Jin <jincheng@ca.ibm.com>

--- jcl/src/java.base/share/classes/openj9/internal/foreign/abi/UpcallMHMetaData.java
+++ jcl/src/java.base/share/classes/openj9/internal/foreign/abi/UpcallMHMetaData.java
@@ -1,6 +1,6 @@
-/*[INCLUDE-IF (JAVA_SPEC_VERSION >= 16) & (JAVA_SPEC_VERSION <= 18) ]*/
+/*[INCLUDE-IF JAVA_SPEC_VERSION >= 19]*/
 /*******************************************************************************
- * Copyright (c) 2021, 2022 IBM Corp. and others
+ * Copyright (c) 2022, 2022 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -20,14 +20,20 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
-package jdk.internal.foreign.abi;
+package openj9.internal.foreign.abi;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodType;
 
+/*[IF JAVA_SPEC_VERSION >= 19]*/
+import java.lang.foreign.MemorySession;
+/*[ELSE] JAVA_SPEC_VERSION >= 19 */
+import jdk.incubator.foreign.ResourceScope;
+/*[ENDIF] JAVA_SPEC_VERSION >= 19 */
+
 /**
  * The meta data consists of the callee MH and a cache of 2 elements for MH resolution,
- * which are used to generate a upcall handler to the requested java method.
+ * which are used to generate an upcall handler to the requested java method.
  */
 final class UpcallMHMetaData {
 
@@ -45,6 +51,12 @@ final class UpcallMHMetaData {
 	 */
 	private Object[] invokeCache;
 
+	/*[IF JAVA_SPEC_VERSION >= 19]*/
+	private MemorySession session;
+	/*[ELSE] JAVA_SPEC_VERSION >= 19 */
+	private ResourceScope scope;
+	/*[ENDIF] JAVA_SPEC_VERSION >= 19 */
+
 	private static synchronized native void resolveUpcallDataFields();
 
 	static {
@@ -55,8 +67,18 @@ final class UpcallMHMetaData {
 		resolveUpcallDataFields();
 	}
 
-	UpcallMHMetaData(MethodHandle targetHandle) {
+	/*[IF JAVA_SPEC_VERSION >= 19]*/
+	UpcallMHMetaData(MethodHandle targetHandle, MemorySession session)
+	/*[ELSE] JAVA_SPEC_VERSION >= 19 */
+	UpcallMHMetaData(MethodHandle targetHandle, ResourceScope scope)
+	/*[ENDIF] JAVA_SPEC_VERSION >= 19 */
+	{
 		calleeMH = targetHandle;
 		calleeType = targetHandle.type();
+		/*[IF JAVA_SPEC_VERSION >= 19]*/
+		this.session = session;
+		/*[ELSE] JAVA_SPEC_VERSION >= 19 */
+		this.scope = scope;
+		/*[ENDIF] JAVA_SPEC_VERSION >= 19 */
 	}
 }
