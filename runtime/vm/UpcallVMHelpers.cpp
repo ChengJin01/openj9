efe43dd34e081a131dfaac9d15f043ec93f3f9ad
ChengJin01 <jincheng@ca.ibm.com>
2023-02-23 12:11:22 -0500

    [FFI] Handling the captured exceptions for the upcall method
    Signed-off-by: ChengJin01 <jincheng@ca.ibm.com>

--- runtime/vm/UpcallVMHelpers.cpp
+++ runtime/vm/UpcallVMHelpers.cpp
@@ -224,7 +224,6 @@ native2InterpJavaUpcallImpl(J9UpcallMetaData *data, void *argsListPointer)
 	J9VMEntryLocalStorage newELS = {0};
 	J9VMThread *currentThread = NULL;
 	bool isCurThrdAllocated = false;
-	bool throwOOM = false;
 	U_64 returnStorage = 0;
 
 	/* Determine whether to use the current thread or create a new one
@@ -249,7 +248,6 @@ native2InterpJavaUpcallImpl(J9UpcallMetaData *data, void *argsListPointer)
 
 		/* Store the allocated memory objects for the struct/pointer arguments to the java array. */
 		if (!storeMemArgObjectsToJavaArray(data, argsListPointer, currentThread)) {
-			throwOOM = true;
 			goto done;
 		}
 
@@ -332,16 +330,22 @@ done:
 	/* Transfer the exception from the locally created upcall thread to the downcall thread
 	 * as the upcall thread will be cleaned up before the dispatcher exits; otherwise
 	 * the current thread's exception can be brought back to the interpreter in downcall.
+	 *
+	 * Note:
+	 * The exception could be OOM which is set for the downcall thread
+	 * in storeMemArgObjectsToJavaArray().
 	 */
-	if (VM_VMHelpers::exceptionPending(currentThread) && isCurThrdAllocated) {
-		downCallThread->currentException = currentThread->currentException;
-		currentThread->currentException = NULL;
-	}
-
-	if (!throwOOM) {
-		/* Read returnStorage from returnValue (and returnValue2 on 32-bit platforms). */
-		returnStorage = *(U_64 *)&currentThread->returnValue;
-		convertUpcallReturnValue(data, returnType, &returnStorage);
+	if (!VM_VMHelpers::exceptionPending(downCallThread)) {
+		if (VM_VMHelpers::exceptionPending(currentThread)) {
+			if (isCurThrdAllocated) {
+				downCallThread->currentException = currentThread->currentException;
+				currentThread->currentException = NULL;
+			}
+		} else {
+			/* Read returnStorage from returnValue (and returnValue2 on 32-bit platforms). */
+			returnStorage = *(U_64 *)&currentThread->returnValue;
+			convertUpcallReturnValue(data, returnType, &returnStorage);
+		}
 	}
 
 	VM_VMAccess::inlineExitVMToJNI(currentThread);
@@ -358,6 +362,16 @@ done:
 	if (isCurThrdAllocated) {
 		threadCleanup(currentThread, false);
 		currentThread = NULL;
+	}
+
+	/* Restore back to the setjump site in the call-out native
+	 * to handle the captured exception.
+	 *
+	 * See inlInternalDowncallHandlerInvokeNative()
+	 * in BytecodeInterpreter.hpp for details.
+	 */
+	if (VM_VMHelpers::exceptionPending(downCallThread)) {
+		longjmp(*(downCallThread->jmpBufferEnv), 1);
 	}
 
 doneAndExit:
